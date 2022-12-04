dcbc4531e9ad33b8d3a41e517c33fd875bcfb646
Vockenroth <judith.vockenroth@gmail.com>
2022-12-04 19:57:01 +0100

    WIP - get information from players formset

--- scorerSheet/views.py
+++ scorerSheet/views.py
@@ -5,7 +5,7 @@ from scorerSheet.forms import CellForm, GameForm, TeamForm, PlayersForm
 from scorerSheet.models import Cell, Player
 
 
-def update_sheet(request, game_id):
+def update_sheet(request, game_id=0):
     # TODO: this view should retrieve the relevant cells for game_id
     CellFormSet = modelformset_factory(Cell, CellForm, extra=0)
     if request.method == 'POST':
@@ -48,20 +48,31 @@ def create_team(request):
 
 
 def add_players(request, game_id):
+    PlayerFormSet = modelformset_factory(Player, PlayersForm, extra=0)
     if request.method == 'POST':
-        return redirect('update_sheet')  # add argument / game id
+        home_team_formset = PlayerFormSet(request.POST, prefix='home_team_formset')
+        print(home_team_formset) # html with form
+
+        print('=======================================')
+
+        for form in home_team_formset:
+            print(form.cleaned_data)
+
+        return redirect('update_sheet')  # todo: add argument / game id
     else:
         PlayerFormSet = modelformset_factory(Player, PlayersForm, extra=0, min_num=9)
-        home_team_formset = PlayerFormSet(request.POST or None)
-        if home_team_formset.is_valid():
-            for form in home_team_formset:
-                print(form.cleaned_data)
-        home_team_formset.save()
 
-        guest_team_formset = PlayerFormSet(request.POST or None)
-        if guest_team_formset.is_valid():
-            for form in guest_team_formset:
-                print(form.cleaned_data)
-        guest_team_formset.save()
+        home_team_formset = create_player_formset(PlayerFormSet, request, 'home_team_formset')
+        guest_team_formset = create_player_formset(PlayerFormSet, request, 'guest_team_formset')
         context = {'home_team_formset': home_team_formset, 'guest_team_formset': guest_team_formset}
+
         return render(request, 'add_players.html', context)
+
+
+def create_player_formset(PlayerFormSet, request, prefix):
+    home_team_formset = PlayerFormSet(request.POST or None, prefix=prefix)
+    if home_team_formset.is_valid():
+        for form in home_team_formset:
+            print(form.cleaned_data)
+    home_team_formset.save()
+    return home_team_formset
