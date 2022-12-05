c69690012600570d557a724cf000628df196b066
Vockenroth <judith.vockenroth@gmail.com>
2022-12-05 21:56:06 +0100

    Refractor name for writing down all team members

--- scorerSheet/urls.py
+++ scorerSheet/urls.py
@@ -5,7 +5,7 @@ from scorerSheet import views
 urlpatterns = [
     path('create_team', views.create_team, name='create_team'),
     path('new_game', views.new_game, name='new_game'),
-    path('add_players/<int:game_id>', views.add_players, name='add_players'),
+    path('create_batting_order/<int:game_id>/<int:team_id>', views.create_batting_order, name='create_batting_order'),
     path('add_player/<int:game_id>/<int:team_id>', views.add_player, name='add_player'),
     path('update_sheet', views.update_sheet, name='update_sheet'),
 ]
--- scorerSheet/views.py
+++ scorerSheet/views.py
@@ -1,8 +1,8 @@
 from django.forms import modelformset_factory
 from django.shortcuts import render, redirect, get_object_or_404
 
-from scorerSheet.forms import CellForm, GameForm, TeamForm, PlayersForm
-from scorerSheet.models import Cell, Player, Game, Score
+from scorerSheet.forms import CellForm, GameForm, TeamForm, PlayerForm
+from scorerSheet.models import Cell, Player, Game, Team
 
 
 def update_sheet(request, game_id=0):
@@ -30,7 +30,7 @@ def new_game(request):
             # Score, BattingOrder and Cell objects, then we redirect to
             # update_sheet yielding the right game
 
-            return redirect('add_players', created_game.id)
+            return redirect('create_batting_order', created_game.id, created_game.home_team.club_number)
     else:
         form = GameForm()
     return render(request, 'new_game.html', {'form': form})
@@ -47,18 +47,17 @@ def create_team(request):
     return render(request, 'create_team.html', {'form': form})
 
 
-def add_players(request, game_id, team_id):
+def create_batting_order(request, game_id, team_id):
     # TODO: this leads to duplicate players, so let's have a simple view for
     # adding players one by one (like create_team view), then this view is
     # really for BattingOrder, which if you put that in modelformset_factory
     # below game and player become dropdowns and user adds position, etc in
     # tabular form -- this new model formset should support editing as well
-    PlayerFormSetHome = modelformset_factory(Player, PlayersForm, min_num=8, max_num=10)
+    PlayerFormSetHome = modelformset_factory(Player, PlayerForm, min_num=8, max_num=10)
     game = get_object_or_404(Game, pk=game_id)
 
     if request.method == 'POST':
-        home_team_formset = PlayerFormSetHome(request.POST, prefix='home_team_formset')
-
+        home_team_formset = PlayerFormSetHome(request.POST)
         if home_team_formset.is_valid():
             for form in home_team_formset:
                 # https://stackoverflow.com/a/29899919
@@ -77,13 +76,16 @@ def add_players(request, game_id, team_id):
     #guest_team_formset = create_player_formset(PlayerFormSet, request, 'guest_team_formset')
     context = {
         'home_team_formset': PlayerFormSetHome,
+        'team_name': game.home_team.team_name,
+        'game_id': game_id,
+        'team_id': team_id,
     }
-    return render(request, 'add_players.html', context)
+    return render(request, 'create_batting_order.html', context)
 
 
 def create_player_formset(PlayerFormSet, request, prefix):
-    # TODO: not much left in this function, can inline in add_players
-    home_team_formset = PlayerFormSet(request.POST or None, prefix=prefix)
+    # TODO: not much left in this function, can inline in create_batting_order
+    home_team_formset = PlayerFormSet(request.POST or None)
     return home_team_formset
 
 
@@ -92,7 +94,7 @@ def add_player(request, game_id, team_id):
         form = PlayerForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('add_players', game_id, team_id)
+            return redirect('create_batting_order', game_id, team_id)
     else:
         team = get_object_or_404(Team, pk=team_id)
         data = {'team': team}
