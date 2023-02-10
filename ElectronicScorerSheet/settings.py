1000d303c74660ed6ff11eea22b7cccd4ba08518
Vockenroth <judith.vockenroth@gmail.com>
2023-02-10 14:49:51 +0100

    Add required login for all app views

--- ElectronicScorerSheet/settings.py
+++ ElectronicScorerSheet/settings.py
@@ -125,5 +125,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "game_board"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_URL = '/user_admin/login/'
+LOGIN_REDIRECT_URL = 'game_board'
+LOGOUT_REDIRECT_URL = 'home'
