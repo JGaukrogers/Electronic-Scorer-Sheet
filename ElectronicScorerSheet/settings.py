491f9a710c53761d098b5deafc55dc275029b1f6
Vockenroth <judith.vockenroth@gmail.com>
2023-09-26 19:44:06 +0200

    Delete not needed files and configuration

--- ElectronicScorerSheet/settings.py
+++ ElectronicScorerSheet/settings.py
@@ -118,11 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
